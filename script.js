7c74f4d6db55a105c7950f4cf0f8a48dd8f82298
truerisa <rmetzner@proton.me>
2025-07-22 11:20:31 +0200

    Calculate

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// Links to HTML elements //
+
 const tbxC1 = document.getElementById("operand1");
 const tbxC2 = document.getElementById("operand2");
 const tbxResult = document.getElementById("result");
 const btnTotal = document.getElementById("btnTotal");
 const btnClear = document.getElementById("btnClear");
+
+// Function to calculate the total of two numbers //
+function calculateTotal() {
+    // Get the values from the input fields and convert them to numbers //
+    let C1 = Number(tbxC1.value);
+    let C2 = Number(tbxC2.value);
+    // Display the result in the result input field //
+    let result = C1 + C2;
+    // Show the result //
+    tbxResult.value = result;
+    
+
+
+}
