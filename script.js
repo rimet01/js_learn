4cfe5dd0aa44bab6930f56ce167488f2d9e7d414
truerisa <rmetzner@proton.me>
2025-07-22 11:16:40 +0200

    JS - links

---
+++ script.js
@@ -0,0 +1,5 @@
+const tbxC1 = document.getElementById("operand1");
+const tbxC2 = document.getElementById("operand2");
+const tbxResult = document.getElementById("result");
+const btnTotal = document.getElementById("btnTotal");
+const btnClear = document.getElementById("btnClear");
