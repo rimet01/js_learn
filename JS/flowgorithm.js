dcb67bf25cbafb100d4e0d0867a95ae3bb6f527a
rimet01 <rmetzner@proton.me>
2025-08-15 21:38:52 +0200

    Random

--- JS/flowgorithm.js
+++ JS/flowgorithm.js
@@ -71,3 +71,4 @@ function main() {
         // Draw a
     }
 }
+
--- JS/random.js
+++ JS/random.js
@@ -4,3 +4,14 @@ let y;
 x = 10;
 y = 20;
 let z = x + y;
+
+- - -
+
+function main() {{
+    var x;
+
+    for (x = 100) {
+        window.alert(x);
+    }
+        
+}
